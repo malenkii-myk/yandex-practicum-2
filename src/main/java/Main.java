2138ff0ddfb460006291947862299c06b2815742
Михаил <malenkii-myk@yandex.ru>
2023-12-12 21:41:45 +0300

    задание выполнено, добавлены классы (menu, tovar, formatter), добавлен формат рублей, проработал ошибки при вводе от пользователя (обернул в try catch).

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,44 @@
 
 import java.util.Scanner;
 
+
+class Menu {
+    String list = "";
+    double totalPrice = 0;
+    int count = 0; // кол-во товаров в списке
+
+    public boolean tovar_add( Tovar tovar) {
+        this.count++;
+        this.list += this.count + " - " + tovar.get_info() + "\r\n";
+        this.totalPrice += tovar.price;
+        return true;
+    }
+
+    public double calculate( int people ) {
+        return this.totalPrice / people;
+    }
+}
+
+class Tovar {
+    String name;
+    double price;
+    public String get_info() {
+        return String.format("%s = %.2f %s", this.name, this.price, new Formatter().roubles(this.price) );
+    }
+}
+
+class Formatter {
+    public String roubles( double value ) {
+        switch ( (int)value % 10 ) {
+            case 1: return "рубль";
+            case 2,3,4: return "рубля";
+            default: return "рублей";
+        }
+    }
+}
+
 public class Main {
+
     public static void main(String[] args) {
 
         System.out.println("Калькулятор счёта");
@@ -9,41 +46,65 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         int peoples = 0;
-        while (peoples < 1) {
+        String userInput = "";
+
+        while (peoples < 2) {
             System.out.println("Укажите кол-во человек:");
-            peoples = scanner.nextInt();
-            if (peoples < 2) {
-                System.out.println("Значение олжно быть больше \"1\"!");
+            userInput = scanner.next();
+            try {
+                peoples = Integer.parseInt(userInput);
+                if (peoples == 1) {
+                    System.out.println("ОШИБКА: для одного человека нет смысла ничего считать!");
+                } else if (peoples < 2) {
+                    System.out.println("ОШИБКА: Значение должно быть больше \"1\"!");
+                }
+            } catch (Exception e){
+                System.out.println("ОШИБКА: Значение должно быть целым числом");
             }
         }
 
-        String menuList = "";
-        double menuPrice = 0;
-        int menuCount = 0;
+        //System.out.println("Количество = "+peoples);
+
+        Menu menu = new Menu();
 
         while (true) {
 
-            String tovarName = "";
-            System.out.println("Укажите название для товара №"+(menuCount+1)+":");
-            tovarName = scanner.next();
+            Tovar tovar = new Tovar();
 
-            double tovarPrice = 0;
-            System.out.println("Укажите цену для товара №"+(menuCount+1)+( tovarName.length() == 0 ? " ("+tovarName+")" : "")+":");
-            tovarPrice = scanner.nextDouble();
+            // название товара
+            System.out.println("Укажите название для товара №"+(menu.count+1)+":");
+            tovar.name = scanner.next();
 
-            menuCount++;
-            menuList += menuCount + " - " + tovarName + " = " + tovarPrice + " руб.\r\n";
-            menuPrice += tovarPrice;
+            // цена товара
+            System.out.println("Укажите цену для товара №"+(menu.count+1)+( tovar.name.length() == 0 ? " ("+tovar.name+")" : "")+":");
+            while (true) {
+                userInput = scanner.next();
+                try {
+                    tovar.price = Double.parseDouble(userInput);
+                    break;
+                } catch (Exception e) {
+                    System.out.println("ОШИБКА: Некорректное значение, попробуйте ещё раз");
+                }
+            }
 
-            System.out.println("Товар №"+menuCount+" ("+ tovarName + " = " + tovarPrice + " руб.) успешно добален.");
-
-            System.out.println("Добавить ещё один товар? Введите любой символ чтобы продолжить. Введите \"Завершить\" чтобы закончить формирование списка.");
-            String cmd = scanner.next();
-            if ( cmd.equalsIgnoreCase( "завершить" )  ) {
-                System.out.println("Формирование списка завершено:");
-                System.out.println( menuList );
+            // добавляем товар в меню
+            if ( menu.tovar_add( tovar )) {
+                System.out.println("Товар №" + menu.count + " (" + tovar.get_info() + ") успешно добален.");
+            } else {
+                System.out.println("ОШИБКА: товар не добавлен");
                 break;
             }
+
+            System.out.println("Добавить следующий товар?\r\nВведите любой символ чтобы продолжить или введите \"Завершить\" чтобы закончить формирование списка.");
+            userInput = scanner.next();
+            if ( userInput.equalsIgnoreCase( "завершить" )  ) {
+                break;
+            }
+        }
+        if (menu.count > 0) {
+            System.out.println("Добавленные товары:");
+            System.out.println( menu.list );
+            System.out.println( String.format( "Каждый должен заплатить по %.2f %s.", menu.calculate(peoples), new Formatter().roubles(menu.calculate(peoples))));
         }
     }
 }
